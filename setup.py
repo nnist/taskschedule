5e0c66ce805229c747bca2d5074ef4734edfbd31
Nicole Nisters <n.nisters@pm.me>
2019-05-22 16:56:42 +0200

    Add setup script

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='taskschedule',
+    version='0.1.0',
+    license='MIT',
+    author='Nicole Nisters',
+    author_email='n.nisters@pm.me',
+    description='A taskwarrior extension to display scheduled tasks.',
+    long_description='A taskwarrior extension to display scheduled tasks.'
+                     'It uses curses to show a table with scheduled tasks. '
+                     'The table is refreshed automatically.',
+    url='https://github.com/nnist/taskschedule',
+    classifiers=[
+        'Environment :: Console :: Curses',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: BSD',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: System :: Shells',
+        'Topic :: Terminals'
+    ],
+    python_requires='>=3.5',
+    packages=[
+        'taskschedule',
+        'taskschedule.tests'
+    ],
+    include_package_data=True,
+    install_requires=[
+        'tabulate',
+        'tasklib',
+        'isodate',
+    ],
+    extras_require={
+        'dev': [
+            'pylint',
+        ]
+    }
+)
