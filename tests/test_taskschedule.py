52c1c85116dda99b6c6be744e9ff23a1c14f55cf
Nicole Nisters <n.nisters@pm.me>
2019-05-22 12:50:28 +0200

    Add dummy tasks to test

--- tests/test_taskschedule.py
+++ tests/test_taskschedule.py
@@ -11,12 +11,16 @@ class TaskscheduleTest(unittest.TestCase):
     def setUp(self):
         taskwarrior = TaskWarrior(data_location='tests/test_data/.task',
                                   create=True)
+        Task(taskwarrior, description='test_yesterday',
+             schedule='yesterday', estimate='20min').save()
         Task(taskwarrior, description='test_9:00_to_10:11',
              schedule='today+9hr', estimate='71min').save()
         Task(taskwarrior, description='test_14:00_to_16:00',
              schedule='today+14hr', estimate='2hr').save()
         Task(taskwarrior, description='test_16:10_to_16:34',
              schedule='today+16hr+10min', estimate='24min').save()
+        Task(taskwarrior, description='test_tomorrow',
+             schedule='tomorrow', estimate='24min').save()
 
         self.schedule = Schedule(tw_data_dir='tests/test_data/.task',
                                  tw_data_dir_create=True)
