4505a77070f0c44a63a292e7cbea1ab04866eb0f
Nicole Nisters <n.nisters@pm.me>
2019-05-22 09:54:40 +0200

    Improve concept

--- concept.py
+++ concept.py
@@ -1,5 +1,9 @@
+from operator import itemgetter
+from datetime import time, datetime, timedelta
+
 from tabulate import tabulate
-from tasklib import Task, TaskWarrior
+from tasklib import TaskWarrior
+from isodate import parse_duration
 
 
 class Schedule():
@@ -8,32 +12,53 @@ class Schedule():
 
     def get_tasks(self):
         taskwarrior = TaskWarrior(data_location='~/.task', create=False)
-        self.tasks = taskwarrior.tasks.filter(scheduled='today')
+        self.tasks = taskwarrior.tasks.filter(scheduled='today',
+                                              status='pending')
+
+    def format_task(self, task):
+        hour = task['scheduled'].hour
+        glyph = '○'
+        task_id = task['id']
+
+        start = task['scheduled']
+        estimate = task['estimate']
+
+        start_time = '{}'.format(start.strftime('%H:%M'))
+
+        if estimate is None:
+            formatted_time = '{}'.format(start_time)
+        else:
+            duration = parse_duration(estimate)
+            end = start + duration
+            end_time = '{}'.format(end.strftime('%H:%M'))
+            formatted_time = '{}-{}'.format(start_time, end_time)
+
+        description = task['description']
+
+        formatted_task = [hour, glyph, task_id, formatted_time, description]
+        return formatted_task
 
     def print_as_table(self):
-        rows = []
+        tasks = self.tasks
+        formatted_tasks = []
 
-        # TODO Sort tasks by hour
-        sorted_tasks = self.tasks
+        for task in tasks:
+            formatted_task = self.format_task(task)
+            formatted_tasks.append(formatted_task)
 
-        # TODO Fill in remaining hours with empty lines
+        hours = []
+        for task in formatted_tasks:
+            hours.append(task[0])
 
-        for task in sorted_tasks:
-            hour = task['scheduled'].hour
-            glyph = '○'
-            task_id = task['id']
+        for i in range(24):
+            if i not in hours:
+                formatted_tasks.append([i, '', '', '', ''])
 
-            # TODO Improve time formatting
-            start = '{}:{}'.format(task['scheduled'].hour, task['scheduled'].minute)
-            end = '{}:{}'.format(task['scheduled'].hour, task['scheduled'].minute)
-            time = '{}-{}'.format(start, end)
-
-            description = task['description']
-            rows.append([hour, glyph, task_id, time, description])
+        sorted_tasks = sorted(formatted_tasks, key=itemgetter(0))
 
         headers = ['', '', '\033[4mID\033[0m', '\033[4mTime\033[0m',
                    '\033[4mDescription\033[0m']
-        print(tabulate(rows, headers, tablefmt="plain"))
+        print(tabulate(sorted_tasks, headers, tablefmt="plain"))
 
 
 schedule = Schedule()
