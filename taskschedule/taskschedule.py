95795de4425eefa03ee769c65a348c327ffe395e
Nicole Nisters <n.nisters@pm.me>
2019-05-22 10:37:03 +0200

    Add test setup and teardown

--- taskschedule/taskschedule.py
+++ taskschedule/taskschedule.py
@@ -1,3 +1,11 @@
+from tasklib import TaskWarrior
+
 class Schedule():
-    def __init__(self):
-        pass
+    def __init__(self, tw_data_dir='~/.task', tw_data_dir_create=False):
+        self.tw_data_dir = tw_data_dir
+        self.tw_data_dir_create = tw_data_dir_create
+
+    def get_tasks(self):
+        taskwarrior = TaskWarrior(self.tw_data_dir, self.tw_data_dir_create)
+        self.tasks = taskwarrior.tasks.filter(scheduled='today',
+                                              status='pending')
--- tests/test_taskschedule.py
+++ tests/test_taskschedule.py
@@ -1,16 +1,40 @@
 import unittest
+import os
 
 from .context import taskschedule
 from taskschedule.taskschedule import Schedule
 
+from tasklib import TaskWarrior, Task
+
 
 class TaskscheduleTest(unittest.TestCase):
     def setUp(self):
-        pass
+        taskwarrior = TaskWarrior(data_location='tests/test_data/.task',
+                                  create=True)
+        Task(taskwarrior, description='test_9:00_to_10:11',
+             scheduled='09:00', estimate='71min').save()
+        Task(taskwarrior, description='test_14:00_to_16:00',
+             scheduled='14:00', estimate='2hr').save()
+        Task(taskwarrior, description='test_16:10_to_16:34',
+             scheduled='16:10', estimate='24min').save()
+
+        self.schedule = Schedule(tw_data_dir='tests/test_data/.task',
+                                 tw_data_dir_create=True)
+
+    def tearDown(self):
+        os.remove(os.path.dirname(__file__) + '/test_data/.task/backlog.data')
+        os.remove(os.path.dirname(__file__) + '/test_data/.task/completed.data')
+        os.remove(os.path.dirname(__file__) + '/test_data/.task/pending.data')
+        os.remove(os.path.dirname(__file__) + '/test_data/.task/undo.data')
 
     def test_schedule_can_be_initialized(self):
         schedule = Schedule()
         assert schedule is not None
+
+    def test_get_tasks_returns_correct_tasks(self):
+        self.schedule.get_tasks()
+        print(self.schedule.tasks)
+        assert self.schedule.tasks is not []
 
 
 if __name__ == '__main__':
