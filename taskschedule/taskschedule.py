46171b8d4f5264325cedca2e18947a4fe7a90401
Nicole Nisters <n.nisters@pm.me>
2019-05-22 14:45:21 +0200

    Use curses for rendering

--- taskschedule/taskschedule.py
+++ taskschedule/taskschedule.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import argparse
+import time
+import curses
 from operator import itemgetter
 
 from tabulate import tabulate
@@ -63,16 +65,42 @@ class Schedule():
         return tabulate(sorted_tasks, headers, tablefmt="plain")
 
 
+def draw(stdscr, refresh_rate=1):
+    schedule = Schedule()
+    curses.curs_set(0)
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(2, 8, 0)
+
+    while True:
+        stdscr.clear()
+
+        schedule.get_tasks()
+        rows = schedule.format_as_table().splitlines()
+        header = rows[0]
+        data = rows[1:]
+
+        stdscr.addstr(0, 0, header, curses.color_pair(1) | curses.A_UNDERLINE)
+
+        for i, row in enumerate(data):
+            stdscr.addstr(i+1, 0, row[:2], curses.color_pair(2))
+            stdscr.addstr(i+1, 2, row[2:], curses.color_pair(1))
+
+        stdscr.refresh()
+        time.sleep(refresh_rate)
+
+
 def main(argv):
     """Display a schedule report for taskwarrior."""
     parser = argparse.ArgumentParser(
         description="""Display a schedule report for taskwarrior."""
     )
+    parser.add_argument(
+        '-r', '--refresh', help="refresh every n seconds", type=int, default=1
+    )
     args = parser.parse_args(argv)
 
-    schedule = Schedule()
-    schedule.get_tasks()
-    print(schedule.format_as_table())
+    curses.wrapper(draw, args.refresh)
 
 
 if __name__ == "__main__":
