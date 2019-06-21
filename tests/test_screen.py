65d96d8479ad28315fe6dac40bd7ce87a3494f5f
Nicole Nisters <n.nisters@pm.me>
2019-06-21 23:58:52 +0200

    Add unit test for Screen

---
+++ tests/test_screen.py
@@ -0,0 +1,63 @@
+# pylint: disable=missing-docstring,no-self-use
+
+import unittest
+import os
+import sys
+from datetime import datetime
+import shutil
+import curses
+
+from tasklib import TaskWarrior, Task
+
+from taskschedule.screen import Screen
+from taskschedule.schedule import TaskDirDoesNotExistError,\
+                                  TaskrcDoesNotExistError
+
+from .context import taskschedule
+
+
+class ScreenTest(unittest.TestCase):
+    def setUp(self):
+        self.taskrc_path = 'tests/test_data/.taskrc'
+        self.task_dir_path = 'tests/test_data/.task'
+        self.assertEqual(os.path.isdir(self.taskrc_path), False)
+        self.assertEqual(os.path.isdir(self.task_dir_path), False)
+
+        # Create a sample .taskrc
+        with open(self.taskrc_path, 'w+') as file:
+            file.write('# User Defined Attributes\n')
+            file.write('uda.estimate.type=duration\n')
+            file.write('uda.estimate.label=Est\n')
+
+        # Create a sample empty .task directory
+        os.makedirs(self.task_dir_path)
+
+    def tearDown(self):
+        try:
+            os.remove(self.taskrc_path)
+        except FileNotFoundError:
+            pass
+
+        try:
+            shutil.rmtree(self.task_dir_path)
+        except FileNotFoundError:
+            pass
+
+    def test_screen_refresh_buffer(self):
+        try:
+            screen = Screen(tw_data_dir=self.task_dir_path,
+                            taskrc_location=self.taskrc_path)
+        except TaskDirDoesNotExistError as err:
+            print('Error: {}'.format(err))
+            sys.exit(1)
+        except TaskrcDoesNotExistError as err:
+            print('Error: {}'.format(err))
+            sys.exit(1)
+        finally:
+            curses.endwin()
+
+        screen.refresh_buffer()
+
+
+if __name__ == '__main__':
+    unittest.main()
