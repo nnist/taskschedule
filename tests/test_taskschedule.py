91fe35b866b7221c265d9cabfad75c30a2f6416a
Nicole Nisters <n.nisters@pm.me>
2019-06-21 21:46:16 +0200

    Use test_data dir for tests

--- tests/test_taskschedule.py
+++ tests/test_taskschedule.py
@@ -16,50 +16,47 @@ from .context import taskschedule
 
 class MissingDataTest(unittest.TestCase):
     def setUp(self):
-        # Make sure ~/.taskrc and ~/.task/ do not exist to prevent damage
-        home = os.path.expanduser("~")
-        self.assertEqual(os.path.isfile(home + '/.taskrc'), False)
-        self.assertEqual(os.path.isdir(home + '/.task'), False)
+        self.taskrc_path = 'test_data/.task'
+        self.task_dir_path = 'test_data/.taskrc'
+        self.assertEqual(os.path.isdir(self.taskrc_path), False)
+        self.assertEqual(os.path.isdir(self.task_dir_path), False)
 
     def tearDown(self):
-        home = os.path.expanduser("~")
         try:
-            os.remove(home + '/.taskrc')
+            os.remove(self.taskrc_path)
         except FileNotFoundError:
             pass
 
         try:
-            shutil.rmtree(home + '/.task')
+            shutil.rmtree(self.task_dir_path)
         except FileNotFoundError:
             pass
 
     def create_schedule(self):
-        schedule = Schedule()
+        schedule = Schedule(tw_data_dir=self.task_dir_path,
+                            taskrc_location=self.taskrc_path)
         schedule.load_tasks()
 
     def test_no_uda_estimate_raises_exception(self):
-        home = os.path.expanduser("~")
-        with open(home + '/.taskrc', 'w+') as file:
+        with open(self.taskrc_path, 'w+') as file:
             file.write('# dummy\n')
-        os.makedirs(home + '/.task')
+        os.makedirs(self.task_dir_path)
         self.assertRaises(UDADoesNotExistError, self.create_schedule)
 
     def test_no_task_dir_raises_exception(self):
-        home = os.path.expanduser("~")
-        with open(home + '/.taskrc', 'w+') as file:
+        with open(self.taskrc_path, 'w+') as file:
             file.write('# User Defined Attributes\n')
             file.write('uda.estimate.type=duration\n')
             file.write('uda.estimate.label=Est\n')
         self.assertRaises(TaskDirDoesNotExistError, self.create_schedule)
-        os.remove(home + '/.taskrc')
+        os.remove(self.taskrc_path)
 
     def test_no_taskrc_raises_exception(self):
-        home = os.path.expanduser("~")
-        os.makedirs(home + '/.task')
+        os.makedirs(self.task_dir_path)
         self.assertRaises(TaskrcDoesNotExistError, self.create_schedule)
 
         try:
-            shutil.rmtree(home + '/.task')
+            shutil.rmtree(self.task_dir_path)
         except FileNotFoundError:
             pass
 
