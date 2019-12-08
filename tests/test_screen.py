dc6c6a4a698be21846ce26b8fcbe4eb2dcd4f1e7
Nicole Nisters <n.nisters@pm.me>
2019-12-08 01:22:57 +0100

    Add unittest

---
+++ tests/test_screen.py
@@ -0,0 +1,21 @@
+from taskschedule.screen import Screen
+
+
+class TestScreen:
+    def test_screen_scroll_up_at_top_is_blocked(self, screen):
+        current_scroll_level = screen.scroll_level
+        screen.scroll(-1)
+        assert current_scroll_level == screen.scroll_level
+
+    def test_screen_scroll_down_and_up(self, screen):
+        current_scroll_level = screen.scroll_level
+        screen.scroll(1)
+        assert current_scroll_level + 1 == screen.scroll_level
+
+        current_scroll_level = screen.scroll_level
+        screen.scroll(-1)
+        assert current_scroll_level - 1 == screen.scroll_level
+
+    def test_prerender_footnote(self, screen: Screen):
+        footnote = screen.prerender_footnote()
+        assert "tasks" in str(footnote)
